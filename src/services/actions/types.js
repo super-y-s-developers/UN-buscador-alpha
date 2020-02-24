4f2aa77cf8e106d3ab7b8a525639e08089898093
Yeliana Torres <yatorresm@unal.edu.co>
2020-02-23 23:03:04 -0500

    business logic code and data retrievement from db

---
+++ src/services/actions/types.js
@@ -0,0 +1,8 @@
+// Retrieve dashboard info
+export const FETCH_COURSES = "FETCH_COURSES";
+export const FETCH_CAREERS = "FETCH_CAREERS";
+
+
+// Retrieve user info
+export const FETCH_USER_PERSONAL_DATA = "FETCH_USER_PERSONAL_DATA";
+export const FETCH_USER_RECORD = "FETCH_USER_RECORD";
