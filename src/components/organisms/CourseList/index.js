076845bd06816f8345ed24db91e2ed77b7015367
Yeliana Torres <yatorresm@unal.edu.co>
2020-02-23 22:59:46 -0500

    create courseList organism

---
+++ src/components/organisms/CourseList/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './styles.css';
+
+import CourseCard from 'components/molecules/CourseCard';
+import Card from 'components/atoms/Card';
+
+function CourseList(props) {
+  return (
+    <div className='course-list'>
+
+      {props.data && props.data.length>0 && props.data.map((d, index) =>
+        <CourseCard
+          key={`${index}-d.id`}
+          id={d.id}
+          name={d.name}
+          typologie={d.typologie}
+          credits={d.credits}
+          onClick={props.handleCourseSelection}
+        />)
+      }
+
+    </div>
+  );
+}
+
+export default CourseList;
