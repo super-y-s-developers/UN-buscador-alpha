bafa0c757770cdfe922306c9237f9c23fd96f922
Yeliana Torres <yatorresm@unal.edu.co>
2020-08-01 14:02:18 -0500

    Fix firstToUppercase function

--- src/services/helpers/index.js
+++ src/services/helpers/index.js
@@ -1,3 +1,3 @@
 export const firstToUpperCase = (string) => {
-  return string.toLowerCase()[0].toUpperCase() + string.slice(1);
+  return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
