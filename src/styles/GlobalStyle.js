4e90f38d85eb082178fa9fb91ce6e8b56f05ea49
Yeliana Torres <yatorresm@unal.edu.co>
2020-08-01 14:02:07 -0500

    Fix body height and html background

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -3,12 +3,14 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`${(props) => `
   // ---------- GENERAL ----------- //
 
-  html, body {
+  html {
     width: 100%;
     height: 100%;
+  }
+
+  html, body {
     margin: 0;
     padding: 0;
-    background-color: white;
   }
 
   body {
