da9e765fe942647f5b7900a0c32d7b0b2c427558
Yeliana Torres <yatorresm@unal.edu.co>
2020-08-01 21:51:54 -0500

    add dummy campus and courses

---
+++ src/dummy/campus.js
@@ -0,0 +1,11 @@
+export default [
+  { value: 'a', text: 'Amazonía' },
+  { value: 'b', text: 'Bogotá' },
+  { value: 'c', text: 'Caribe' },
+  { value: 'd', text: 'De La Paz' },
+  { value: 'ma', text: 'Manizales' },
+  { value: 'me', text: 'Medellín' },
+  { value: 'o', text: 'Orinoquía' },
+  { value: 'p', text: 'Palmira' },
+  { value: 't', text: 'Timaco' },
+]
